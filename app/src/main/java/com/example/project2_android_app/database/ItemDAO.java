8678c1afd20cbab21faef94f31e77c1f24791142
Himster29 <hyapa@csumb.edu>
2026-04-28 21:11:49 -0700

    Add Item domain and wire ListActivity to repository part 1

---
+++ app/src/main/java/com/example/project2_android_app/database/ItemDAO.java
@@ -0,0 +1,43 @@
+package com.example.project2_android_app.database;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.project2_android_app.database.entities.Item;
+
+import java.util.List;
+
+/**
+ * Data Access Object for the Item table.
+ *
+ * Author: Himansu Yapa
+ */
+@Dao
+public interface ItemDAO {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    long insert(Item item);
+
+    @Update
+    void update(Item item);
+
+    @Delete
+    void delete(Item item);
+
+    @Query("SELECT * FROM " + Item.TABLE_NAME + " WHERE itemId = :id LIMIT 1")
+    Item getItemById(int id);
+
+    @Query("SELECT * FROM " + Item.TABLE_NAME + " WHERE listId = :listId ORDER BY isBought ASC, itemName ASC")
+    LiveData<List<Item>> getItemsForList(int listId);
+
+    @Query("SELECT * FROM " + Item.TABLE_NAME + " WHERE listId = :listId")
+    List<Item> getItemsForListSync(int listId);
+
+    @Query("SELECT COUNT(*) FROM " + Item.TABLE_NAME + " WHERE listId = :listId")
+    int countItemsForList(int listId);
+}
